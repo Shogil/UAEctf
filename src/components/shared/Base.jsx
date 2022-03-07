8793f398bb95d737308db1da6b084a86a3c7f2b5
Rahul M <rahulmohan255@gmail.com>
2022-03-07 18:41:12 +0530

    file added

--- src/components/shared/Base.jsx
+++ src/components/shared/Base.jsx
@@ -35,15 +35,15 @@ const Base = ({ children, meta }) => {
             {   meta && meta.image && <meta property="og:image" content={meta.image} /> }
             <meta name="viewport" content="width=device-width, minimum-scale=1, shrink-to-fit=no, initial-scale=1" />
             <link rel="manifest" href="/manifest.json" />
-            <link href='/images/icons/icon-72x72.png' rel='icon' type='image/png' sizes='72x72' />
-            <link href='/images/icons/icon-96x96.png' rel='icon' type='image/png' sizes='96x96' />
-            <link href='/images/icons/icon-128x128.png' rel='icon' type='image/png' sizes='128x128' />
-            <link href='/images/icons/icon-152x152.png' rel='icon' type='image/png' sizes='144x144' />
-            <link href='/images/icons/icon-152x152.png' rel='icon' type='image/png' sizes='152x152' />
-            <link href='/images/icons/icon-192x192.png' rel='icon' type='image/png' sizes='192x192' />
-            <link href='/images/icons/icon-384x384.png' rel='icon' type='image/png' sizes='384x384' />
-            <link rel='apple-touch-icon' href='/images/icons/icon-512x512.png' />
-            <link rel="shortcut icon" href="../images/icons/icon-72x72.png" />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='72x72' />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='96x96' />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='128x128' />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='144x144' />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='152x152' />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='192x192' />
+            <link href='/images/icons/inctf_light.png' rel='icon' type='image/png' sizes='384x384' />
+            <link rel='apple-touch-icon' href='/images/icons/inctf_light.png' />
+            <link rel="shortcut icon" href="../images/icons/inctf_light.png" />
             {   GoogleAnalyticsID && <React.Fragment>
                 <script rel="preconnect" async src={`https://www.googletagmanager.com/gtag/js?id=${GoogleAnalyticsID}`} />
                 <script dangerouslySetInnerHTML={{
